42cd6aef9b54f197b30a212053694fc37c9d8e21
Chris Wood <wchristopherwood912@gmail.com>
2023-04-11 09:28:09 -0700

    finished longest substring

--- longest-substring.py
+++ longest-substring.py
@@ -2,30 +2,38 @@
 # substring
 # without repeating characters.
 
+ 
+
+# Example 1:
+
 # Input: s = "abcabcbb"
 # Output: 3
 # Explanation: The answer is "abc", with the length of 3.
 
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
+# Example 2:
+
+# Input: s = "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+
+
+def lengthOfLongestSubstring(self, s: str) -> int:
+        # store the character and the index in s once seen
         seen = {}
-        l = 0 #how many times a character can repeat
+        l = 0
         output = 0
         for r in range(len(s)):
-            """
-            if the substring is not in the dictionary increase the number of characters checked at a time
-            """
+            # check if character is in seen dict
             if s[r] not in seen:
+                # output set to current index or the previous output whatever is greater 
                 output = max(output,r-l+1)
-                """
-                There are two cases if s[r] in seen:
-                case1: s[r] is inside the current window, we need to change the window by moving left pointer to seen[s[r]] + 1.
-                case2: s[r] is not inside the current window, we can keep increase the window
-                """
             else:
                 if seen[s[r]] < l:
+                    # if above happens there are characters separating the two repeating characters and output is adjusted
                     output = max(output,r-l+1)
                 else:
+                    # if this code is run the repeating characters are side by side and output is adjusted accordingly
                     l = seen[s[r]] + 1
+            # adds the character to dict
             seen[s[r]] = r
         return output
